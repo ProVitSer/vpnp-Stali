174b8b3fcf52bcf3b9329756946a57f02b9eaf82
ProVitSer <prokinvs@gmail.com>
2022-09-04 22:28:18 +0300

    docker service

---
+++ src/docker/docker.module.ts
@@ -0,0 +1,26 @@
+import * as Docker from 'dockerode';
+import { DockerService } from './docker.service';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from '@app/logger/logger.module';
+
+const providers = [
+  DockerService,
+  {
+    provide: 'DOCKER_SERVICE',
+    useFactory: (configService: ConfigService) => {
+        return new Docker();
+    },
+    inject: [ConfigService]
+  },
+  
+];
+
+@Module({
+  providers,
+  imports:[ConfigModule, LoggerModule],
+  exports: [...providers],
+})
+
+export class DockerModule {}
+
