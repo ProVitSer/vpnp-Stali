4b65880d052d5451c97d8a246770ebb9f9349f40
ProVitSer <prokinvs@gmail.com>
2022-09-11 19:39:11 +0300

    Постоянная проверка работоспособности сервисов

--- src/schedule/schedule.module.ts
+++ src/schedule/schedule.module.ts
@@ -5,9 +5,12 @@ import { ConfigModule } from '@nestjs/config';
 import { SelenoidModule } from '@app/selenoid/selenoid.module';
 import { MongoModule } from '@app/database/mongo/mongo.module';
 import { ChangeForwardScheduleService } from './schedules/changeForward.schedule';
+import { HealthScheduledService } from './schedules/healthServiceScheduled';
+import { HealthModule } from '@app/health/health.module';
+import { MailModule } from '@app/mail/mail.module';
 
 @Module({
-    imports:[Schedule.forRoot(), LoggerModule, ConfigModule, SelenoidModule, MongoModule],
-    providers: [ChangeForwardScheduleService]
+    imports:[Schedule.forRoot(), LoggerModule, ConfigModule, SelenoidModule, MongoModule, HealthModule, MailModule],
+    providers: [ChangeForwardScheduleService, HealthScheduledService]
 })
 export class ScheduleModule {}
