4ed0a313391df70522883e3d8a02bf8e0cef3e76
ProVitSer <prokinvs@gmail.com>
2023-04-19 00:19:51 +0300

    feat: get current exten mobile

--- src/pbx3cx/pbx3cx-forward-status.service.ts
+++ src/pbx3cx/pbx3cx-forward-status.service.ts
@@ -2,7 +2,7 @@ import { LoggerService } from '@app/logger/logger.service';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Dn, Extension, Fwdprofile } from './entities';
+import { Dn, Dnprop, Extension, Fwdprofile } from './entities';
 
 @Injectable()
 export class Pbx3cxForwardStatusService {
@@ -15,6 +15,8 @@ export class Pbx3cxForwardStatusService {
     private extension: Repository<Extension>,
     @InjectRepository(Fwdprofile)
     private fwdprofile: Repository<Fwdprofile>,
+    @InjectRepository(Dnprop)
+    private dnprop: Repository<Dnprop>,
   ) {
     this.serviceContext = Pbx3cxForwardStatusService.name;
   }
@@ -56,6 +58,22 @@ export class Pbx3cxForwardStatusService {
         .createQueryBuilder('fwdprofile')
         .select('fwdprofile.profilename')
         .where('fwdprofile.idfwdprofile = :id', { id: profileId })
+        .getOne();
+    } catch (e) {
+      this.logger.error(e, this.serviceContext);
+      throw e;
+    }
+  }
+
+  public async getCurrentExtenMobile(dnExtenId: number) {
+    try {
+      return await this.dnprop
+        .createQueryBuilder('dnprop')
+        .select('dnprop.value')
+        .where('dnprop.fkiddn = :id', { id: dnExtenId })
+        .andWhere('dnprop.name like :name', {
+          name: 'MOBILENUMBER',
+        })
         .getOne();
     } catch (e) {
       this.logger.error(e, this.serviceContext);
