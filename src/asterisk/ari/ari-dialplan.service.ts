79469d3c750bbb5b828f36c87d8d6d6a51857a32
ProVitSer <prokinvs@gmail.com>
2023-05-07 22:43:30 +0300

    ref: del no used, add const var

--- src/asterisk/ari/ari-dialplan.service.ts
+++ src/asterisk/ari/ari-dialplan.service.ts
@@ -1,21 +1,20 @@
 import { ContextDialplanNumberMap } from '@app/config/config';
 import { LoggerService } from '@app/logger/logger.service';
 import { Soap1cProvider } from '@app/soap1c/sopa1c.provider';
-import { ReturnNumberResponseData } from '@app/soap1c/interfaces/soap1c.interface';
+import { ReturnNumberResponseData, Soap1cApiRequestInterface } from '@app/soap1c/interfaces/soap1c.interface';
 import { Soap1cActionTypes, Soap1cEnvelopeTypes } from '@app/soap1c/interfaces/soap1c.enum';
 import { UtilsService } from '@app/utils/utils.service';
 import { Inject, Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Ari, { StasisStart } from 'ari-client';
-import { DEFAULT_ROUTING, LOCAL_ROUTING } from '../asterisk.constants';
+import { DEFAULT_NOT_FOUND_EXTENSION, DEFAULT_ROUTING, E1_GATEWAY, LOCAL_ROUTING } from '../asterisk.constants';
 import { RouteInfo } from '../types/interface';
 import * as moment from 'moment';
+import { CALL_DATE_TIME_FORMAT } from '@app/soap1c/soap1c.constants';
 
 @Injectable()
 export class DialplanApplicationService implements OnApplicationBootstrap {
-  // eslint-disable-next-line prettier/prettier
   private client: { ariClient: Ari.Client };
-  private dialTrunk = {};
   private serviceContext: string;
   constructor(
     @Inject('ARI') private readonly ari: { ariClient: Ari.Client },
@@ -27,21 +26,23 @@ export class DialplanApplicationService implements OnApplicationBootstrap {
   }
 
   public async onApplicationBootstrap() {
-    this.client = this.ari;
-    this.client.ariClient.on('StasisStart', async (event: StasisStart) => {
-      try {
-        this.logger.info(`Событие входящего вызова ${JSON.stringify(event)}`, this.serviceContext);
-        const routeInfo = await this.getRouteInfo(event);
-        if (routeInfo.returnDialExtension != '000') {
-          await this.continueDialplan(routeInfo.channelId, LOCAL_ROUTING, routeInfo.returnDialExtension);
-        } else {
-          await this.continueDialplan(routeInfo.channelId, DEFAULT_ROUTING, routeInfo.returnDialExtension);
+    if (!process.env.NODE_APP_INSTANCE || Number(process.env.NODE_APP_INSTANCE) === 0) {
+      this.client = this.ari;
+      this.client.ariClient.on('StasisStart', async (event: StasisStart) => {
+        try {
+          this.logger.info(`Событие входящего вызова ${JSON.stringify(event)}`, this.serviceContext);
+          const routeInfo = await this.getRouteInfo(event);
+          if (routeInfo.returnDialExtension != DEFAULT_NOT_FOUND_EXTENSION) {
+            await this.continueDialplan(routeInfo.channelId, LOCAL_ROUTING, routeInfo.returnDialExtension);
+          } else {
+            await this.continueDialplan(routeInfo.channelId, DEFAULT_ROUTING, routeInfo.returnDialExtension);
+          }
+        } catch (e) {
+          this.logger.info(` Error ARI continueDialplan ${e}`, this.serviceContext);
         }
-      } catch (e) {
-        this.logger.info(` Error ARI continueDialplan ${e}`, this.serviceContext);
-      }
-    });
-    this.client.ariClient.start(this.configService.get('asterisk.ari.application.amocrm'));
+      });
+      this.client.ariClient.start(this.configService.get('asterisk.ari.application.app'));
+    }
   }
 
   private async continueDialplan(returnChannelId: string, dialplanContext: string, returnDialExtension: string): Promise<string> {
@@ -50,8 +51,6 @@ export class DialplanApplicationService implements OnApplicationBootstrap {
         `Перенаправляем вызов в по нужному маршруту ${returnChannelId}  ${dialplanContext}  ${returnDialExtension}`,
         this.serviceContext,
       );
-      this.logger.info(JSON.stringify(this.dialTrunk), this.serviceContext);
-      delete this.dialTrunk[returnChannelId];
       return await new Promise((resolve, reject) => {
         this.client.ariClient.channels.continueInDialplan(
           {
@@ -73,24 +72,12 @@ export class DialplanApplicationService implements OnApplicationBootstrap {
 
   private async getRouteInfo(event: StasisStart): Promise<RouteInfo> {
     try {
-      const incomingNumber = UtilsService.formatNumber(event.channel.caller.number);
-      const dialedNumber = this.getDialExtension(event);
-      this.logger.info(`${incomingNumber} ${dialedNumber} ${event.channel.id}`);
-      const requestInfo = {
-        action: Soap1cActionTypes.getRouteNumber,
-        envelop: Soap1cEnvelopeTypes.returnNumber,
-        data: {
-          incomingNumber,
-          dialedNumber,
-          channelId: event.channel.id,
-          callDateTime: moment().format('YYYY-MM-DDTHH:mm:ss'),
-        },
-      };
-
+      const requestInfo = this.getSoapRequestInfo(event);
+      this.logger.info(`${JSON.stringify(requestInfo)}`);
       const routeInfo = await this.soap1c.request<ReturnNumberResponseData>(requestInfo);
       const parseRouteInfo = routeInfo.Envelope.Body.ReturnNumberResponse.return.name.split(';');
       return {
-        dialedNumber,
+        dialedNumber: requestInfo.data.dialedNumber,
         returnDialExtension: parseRouteInfo[0],
         channelId: parseRouteInfo[1],
       };
@@ -101,14 +88,26 @@ export class DialplanApplicationService implements OnApplicationBootstrap {
     }
   }
 
+  private getSoapRequestInfo(event: StasisStart): Soap1cApiRequestInterface {
+    const incomingNumber = UtilsService.formatNumber(event.channel.caller.number);
+    const dialedNumber = this.getDialExtension(event);
+    this.logger.info(`${incomingNumber} ${dialedNumber} ${event.channel.id}`);
+    return {
+      action: Soap1cActionTypes.getRouteNumber,
+      envelop: Soap1cEnvelopeTypes.returnNumber,
+      data: {
+        incomingNumber,
+        dialedNumber,
+        channelId: event.channel.id,
+        callDateTime: moment().format(CALL_DATE_TIME_FORMAT),
+      },
+    };
+  }
+
   private getDialExtension(event: StasisStart): string {
-    if (event.channel.dialplan.context == 'beronet') {
-      this.dialTrunk[event.channel.id] = { context: event.args[0] };
+    if (event.channel.dialplan.context == E1_GATEWAY) {
       return event.args[0];
     } else {
-      this.dialTrunk[event.channel.id] = {
-        context: event.channel.dialplan.context,
-      };
       return ContextDialplanNumberMap[event.channel.dialplan.context];
     }
   }
