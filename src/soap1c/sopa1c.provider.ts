4e76af53755a5c742816a3aa6debdf8d3d5577cf
ProVitSer <prokinvs@gmail.com>
2023-05-07 23:00:27 +0300

    feat: новые методы Soap1c , rx для http

--- src/soap1c/sopa1c.provider.ts
+++ src/soap1c/sopa1c.provider.ts
@@ -1,31 +1,33 @@
 import { XmlService } from '@app/xml/xml.service';
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { ReturnNumber } from './providers/return-number';
-import { SetID } from './providers/set-id';
 import { NAMESPACE } from './soap1c.constants';
 import { PlainObject, Soap1cApiRequestInterface, Soap1cProviderInterface, Soap1cProviders } from './interfaces/soap1c.interface';
 import { Soap1cActionTypes, Soap1cEnvelopeTypes } from './interfaces/soap1c.enum';
-import { SetNumber } from './providers/set-number';
+import { GetSoap1cUrl } from './services/get-soap1c-url.service';
+import { LoggerService } from '@app/logger/logger.service';
+import { catchError, firstValueFrom } from 'rxjs';
+import { AxiosError } from '@nestjs/terminus/dist/errors/axios.error';
+import { GetRouteNumber, SetID, SetNumber } from './providers';
 
 @Injectable()
 export class Soap1cProvider {
   private serviceContext: string;
   constructor(
-    private readonly configService: ConfigService,
     private readonly xml: XmlService,
-    private readonly returnNumber: ReturnNumber,
+    private readonly getRouteNumber: GetRouteNumber,
     private readonly setID: SetID,
     private readonly setNumber: SetNumber,
-    private httpService: HttpService,
+    private readonly httpService: HttpService,
+    private readonly soap1cUrl: GetSoap1cUrl,
+    private readonly logger: LoggerService,
   ) {
     this.serviceContext = Soap1cProvider.name;
   }
 
   private get providers(): Soap1cProviders {
     return {
-      [Soap1cActionTypes.getRouteNumber]: this.returnNumber,
+      [Soap1cActionTypes.getRouteNumber]: this.getRouteNumber,
       [Soap1cActionTypes.setID]: this.setID,
       [Soap1cActionTypes.setNumber]: this.setNumber,
     };
@@ -38,9 +40,16 @@ export class Soap1cProvider {
     try {
       const requestData = await provider.getRequestData(data);
       const xmlRequest = await this.makeXmlRequest(requestData, envelop || Soap1cEnvelopeTypes.returnNumber, action);
-      const response = await this.httpService.post(this.configService.get('server1C.url'), xmlRequest).toPromise();
+      const response = await firstValueFrom(
+        this.httpService.post(this.soap1cUrl.get(data), xmlRequest).pipe(
+          catchError((error: AxiosError) => {
+            throw error;
+          }),
+        ),
+      );
       return (await this.xml.createObjectFromXmlAsync(response.data)) as T;
     } catch (e) {
+      this.logger.error(e, this.serviceContext);
       throw e;
     }
   }
