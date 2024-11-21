1570a57a6ca363db389f3ebd5862f40f5b976383
Vitaliy <prokinvs@gmail.com>
2024-11-21 11:29:26 +0100

    Update asterisk.constants.ts

--- src/asterisk/asterisk.constants.ts
+++ src/asterisk/asterisk.constants.ts
@@ -1,4 +1,4 @@
 export const LOCAL_ROUTING = 'RouteToLocalUser';
 export const DEFAULT_ROUTING = 'RouteToDefaultIncomingRoute';
-export const E1_GATEWAY = 'beronet';
+export const E1_GATEWAY = 'nauka-in';
 export const DEFAULT_NOT_FOUND_EXTENSION = '000';
